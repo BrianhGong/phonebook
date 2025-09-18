3fcf0512078acce3a7fc1733511a3a3cc0531d2a
Brian Gong <briangong0049@gmail.com>
2025-09-17 21:10:29 -0500

    initial

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+app.use(express.static('dist'))
+app.use(express.json())
+
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+
+
+let phonebook = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(phonebook)
+})
+
+app.get('/info', (req, res) => {
+    const count = phonebook.length
+    const time = new Date()
+    res.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${time}</p>
+    `)
+
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = phonebook.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+    const id = Math.floor(Math.random() * 100000)
+    const body = req.body
+    if (!body.name || !body.number) {
+        return res.status(400).json({error: 'name and number required'})
+    }
+    if (phonebook.some(person => person.name == body.name)) {
+        return res.status(400).json({error: 'name must be unique'})
+    }
+    
+    const person = {
+        name :body.name,
+        number: body.number,
+        id: id
+    }
+    phonebook = phonebook.concat(person)
+    res.json(person)
+
+})
+
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
